23987b9a8d57a35ad7fba2f70860101b72de3056
wankhedednyandeep55-byte <wankhedednyandeep55@gmail.com>
2026-02-17 18:51:52 +0530

    Create engine-base.js

---
+++ engine-base.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+    'https://appwsjmmmaifgvjspdzs.supabase.co', 
+    'sb_publishable_TkPdkJvzEVTVXUE21k_jFA_614TyGKm'
+);
